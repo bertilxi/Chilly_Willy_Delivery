4945ae9de409fd497f52c624d307df46347ac831
Andres <andres.rico94@gmail.com>
2017-02-16 00:23:00 -0300

    undo changes made for tests

--- mobile/app/src/main/java/dam/isi/frsf/utn/edu/ar/delivery/task/AlarmSetter.java
+++ mobile/app/src/main/java/dam/isi/frsf/utn/edu/ar/delivery/task/AlarmSetter.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 
 import java.util.Calendar;
-import java.util.Date;
 
 import dam.isi.frsf.utn.edu.ar.delivery.R;
 import dam.isi.frsf.utn.edu.ar.delivery.broacast_receiver.AlarmReceiver;
@@ -19,16 +18,11 @@ public class AlarmSetter {
         Intent intent = new Intent(context, AlarmReceiver.class);
         int id = context.getResources().getInteger(R.integer.alarm_intent_id);
         PendingIntent pi = PendingIntent.getBroadcast(context, id, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        /**
         calendar.set(Calendar.DAY_OF_WEEK, Calendar.SATURDAY);
         calendar.set(Calendar.HOUR_OF_DAY, 16);
         calendar.set(Calendar.MINUTE, 0);
         calendar.set(Calendar.SECOND, 0);
-         **/
-        long t= calendar.getTimeInMillis();
-
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        alarmManager.setRepeating(AlarmManager.RTC, t+15000, AlarmManager.INTERVAL_DAY * 7, pi);
+        alarmManager.setRepeating(AlarmManager.RTC, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY * 7, pi);
     }
 }
